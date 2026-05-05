8dd66d5bad3b3aac4d131f2b9333ccc1f544b525
andresmartinezr195-del <andres.martinez.r195@gmail.com>
2026-05-05 17:27:39 -0500

    commit1

---
+++ Avance-Com-practico.py
@@ -0,0 +1,13 @@
+# Curso: Programación
+# Grupo: 213023_240
+
+# importamos las librerias sujeridas por el docente en el grupo de teams
+
+import tkinter as tk                          # Librería principal para la ventana y widgets
+from tkinter import ttk, messagebox, scrolledtext  # Widgets de cuadros de diálogo y texto
+import datetime                               # Para obtener fecha y hora actuales de nuestro pc
+import re                                     # Para validar correos con expresiones regulares
+import os                                     # Para obtener rutas absolutas del sistema
+from abc import ABC, abstractmethod           # Para definir clases y métodos abstractos
+
+
